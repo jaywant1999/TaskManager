0bf704f9114a997079d2fe52c1c4fc0f018c7529
jaywant1999 <153539866+jaywant1999@users.noreply.github.com>
2026-01-14 15:39:12 +0530

    initializing project in github

---
+++ src/main/java/com/TaskManager/TaskManager/HelloController.java
@@ -0,0 +1,13 @@
+package com.TaskManager.TaskManager;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	String hello() {
+		return "Hello I Am Task MAnager";
+	}
+}
